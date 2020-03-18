fa9bfffd458c6e271178b79b6fc58d120ee4ae04
kaining <xkn1994@gmail.cin>
2020-03-18 13:31:33 +0100

    homework v2

--- src/js/js2.js
+++ src/js/js2.js
@@ -1,12 +1,23 @@
 function add() {
     let val = document.getElementById("input_text").value;
     let ul = document.getElementById("shopping-list");
+    let important = document.getElementById("important").checked;
+    let not_important = document.getElementById("not-important").checked;
     if (val){
         let element= document.createElement("li");
+        if (important){
+            element.style.color="red";
+        }
+        if (not_important){
+            element.style["text-decoration"]="underline";
+        }
+
         element.appendChild(document.createTextNode(val));
         ul.appendChild(element);
     }
     document.getElementById("input_text").value = "";
+    document.getElementById("important").checked = false;
+    document.getElementById("not-important").checked =false;
 }
 
 function remove(){
